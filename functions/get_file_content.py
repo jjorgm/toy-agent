b6cb3ec68d26d4337e0daaa2c717f7ab7d794264
jjorgm <jordangarciamoquete@gmail.com>
2026-08-12 20:23:20 -0400

    get file functionality

---
+++ functions/get_file_content.py
@@ -0,0 +1,21 @@
+import os
+
+from config import MAX_CHARS
+
+
+def get_file_content(working_directory: str, file_path: str) -> str:
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_path = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_path]) == working_dir_abs
+        if not valid_target_dir:
+            raise ValueError(f"Error: Cannot read {file_path} as it is outside the permitted working directory")
+        if not os.path.isfile(target_path):
+            raise ValueError(f"Error: File is not found or is not a regular file: {file_path}")
+        with open(target_path, 'r') as file:
+            content = file.read(MAX_CHARS)
+            if file.read(1):
+                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+            return content
+    except (OSError, ValueError) as e:
+        return f"Error: {e!s}"
